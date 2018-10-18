fd46afac46521c3113554e0559feac6f0aff5360
gameDevSteve <steveclarktheartist@gmail.com>
2018-10-18 19:07:06 -0400

    First commit baby

---
+++ simple-restaurant-menu.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Simple Restaurant Menu
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 0.0.1
+ * Author: Steve Clark
+ * Author URI: https://beardbraingames.com
+ */
